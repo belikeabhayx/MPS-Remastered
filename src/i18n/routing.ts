5006c002fb45febcf5ef06cbd93323d163b37594
Abhay Chauhan <chauhanabhay.chauhan2@gmail.com>
2026-04-24 12:15:13 +0530

    feat(footer): add i18n routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -30,6 +30,42 @@ export const routing = defineRouting({
       de: '/blogs/[slug]',
       nl: '/blogs/[slug]',
     },
+    "/privacy-policy": {
+      en: '/privacy-policy',
+      nl: '/privacybeleid',
+      de: '/datenschutzbestimmungen',
+      es: '/politica-de-privacidad',
+    },
+    "/terms-conditions": {
+      en: '/terms-conditions',
+      nl: '/algemene-voorwaarden',
+      de: '/bedingungen-und-konditionen',
+      es: '/condiciones-generales',
+    },
+    "/payment-delivery": {
+      en: '/payment-delivery',
+      nl: '/betaling-levering',
+      de: '/zahlung-lieferung',
+      es: '/pago-y-entrega',
+    },
+    "/right-of-withdrawal": {
+      en: '/right-of-withdrawal',
+      nl: '/herroepingsrecht',
+      de: '/recht-auf-widerruf',
+      es: '/derecho-de-desistimiento',
+    },
+    "/legal-notice": {
+      en: '/legal-notice',
+      nl: '/wettelijke-kennisgeving',
+      de: '/rechtlicher-hinweis',
+      es: '/aviso-legal',
+    },
+    "/cookie-policy": {
+      en: '/cookie-policy',
+      nl: '/cookiebeleid',
+      de: '/cookie-richtlinie',
+      es: '/politica-de-cookies',
+    }
   },
 });
 
